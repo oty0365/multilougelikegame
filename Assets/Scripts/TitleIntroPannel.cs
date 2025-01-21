c37fc9dfc1dd5a5f95667ad88ffe32b024a63525
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-01-21 23:31:16 +0900

    titleScene

--- Assets/Scripts/TitleIntroPannel.cs
+++ Assets/Scripts/TitleIntroPannel.cs
@@ -1,7 +1,21 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class TitleIntroPannel : MonoBehaviour
 {
     [SerializeField] private GameObject titlePannel;
-    public void OnClicked() => titlePannel.SetActive(false);
+    [SerializeField] private GameObject multiPannel;
+    [SerializeField] private GameObject loginPannel;
+
+    private void Start()
+    {
+        multiPannel.SetActive(false);
+        loginPannel.SetActive(false);
+    }
+    public void OnClicked()
+    {
+        titlePannel.SetActive(false);
+        multiPannel.SetActive(true);
+        loginPannel.SetActive(true);
+    }
 }

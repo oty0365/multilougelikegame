a5d5ff10f9fef5bcdfc8551b2e3e2985209fe57c
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-02-01 01:03:52 +0900

    animationrpc

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerStatus : NetworkBehaviour
 {
     [SerializeField] private Animator ani;
+    private NetworkVariable<string> _playableType = new NetworkVariable<string>("", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     async void Start()
     {
         if (IsOwner)
@@ -14,10 +16,22 @@ public class PlayerStatus : NetworkBehaviour
                 if (i.playerCode == DataManager.instance.ServerData["CurrentPlayableCharacter"].Value.GetAsString())
                 {
                     SwitchToSpecialIdle(i.moveSets[0]);
+                    _playableType = new NetworkVariable<string>(i.playerCode, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+                }
+            }
+        }
+        else
+        {
+           foreach(var i in DataManager.instance.playableSets.playables)
+            {
+                if (i.playerCode == _playableType.Value)
+                {
+                    SwitchToSpecialIdle(i.moveSets[0]);
                 }
             }
         }
     }
+
     public void SwitchToSpecialIdle(AnimationClip newClip)
     {
 

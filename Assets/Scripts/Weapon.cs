8c10f37bd52e8edc8fee7e7d6dbb3e2d953e1e3e
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-02-05 20:35:05 +0900

    wip

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Security.Cryptography.X509Certificates;
 using Unity.Netcode;
 using Unity.Netcode.Components;
@@ -7,9 +8,50 @@ public class Weapon : NetworkBehaviour
 {
     public WeaponData weaponData;
     public Animator ani;
-    public int comboRange;
-    protected void OnAttack()
+    public int maxComboRange;
+    protected int curComboRange;
+    public bool isAttacking;
+    private IEnumerator prevCombo;
+    protected virtual void OnBasicAttack()
     {
-        
+        if (!isAttacking)
+        {
+            if (curComboRange == 0)
+            {
+                ani.SetTrigger("attack1");
+                    
+            }
+            else if (curComboRange == 1)
+            {
+                ani.SetTrigger("attack2");
+                 
+            }
+            else if (curComboRange == 2)
+            {
+                ani.SetTrigger("attack3");
+                curComboRange = 0;
+            }
+            if (curComboRange + 1 <= maxComboRange)
+            {
+                curComboRange++;
+            }
+        }
+
+
+    }
+    private IEnumerator ComboFlow()
+    {
+        yield return new WaitForSeconds(0.3f);
+        curComboRange = 0;
+    }
+    public void EndAttack()
+    {
+        isAttacking = false;
+        if(prevCombo != null)
+        {
+            StopCoroutine(prevCombo);
+        }
+        prevCombo = ComboFlow();
+        StartCoroutine(prevCombo);
     }
 }
---
+++ Assets/Scripts/WeaponSlot.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WeaponSlot : MonoBehaviour
+{
+    public GameObject 
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
